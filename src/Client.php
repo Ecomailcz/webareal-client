4388e3b5d818d650e0d72eb82dfc8e20a9be9a4c
Jakub Bouček <pan@jakubboucek.cz>
2020-05-12 04:27:00 +0200

    Client refactoring

--- src/Client.php
+++ src/Client.php
@@ -4,139 +4,211 @@ declare(strict_types=1);
 
 namespace EcomailWebareal;
 
-use EcomailShoptet\Exception\EcomailShoptetRequestError;
-use EcomailWebareal\Exception\EcomailWebarealInvalidAuthorization;
-use EcomailWebareal\Exception\EcomailWebarealNotFound;
+use EcomailWebareal\Exception\AccessDeniedException;
+use EcomailWebareal\Exception\ConnectionException;
+use EcomailWebareal\Exception\InvalidRequestException;
+use EcomailWebareal\Exception\InvalidResponseException;
+use EcomailWebareal\Exception\NotFoundException;
+use EcomailWebareal\Exception\ResponseErrorException;
+use EcomailWebareal\Exception\UnauthorizedException;
+use EcomailWebareal\Exception\UnexpectedResponseException;
+use EcomailWebareal\Response\JsonResponse;
+use EcomailWebareal\Response\Response;
+use EcomailWebareal\TokenCache\ITokenCache;
+use EcomailWebareal\TokenCache\MemoryCache;
+use JsonException;
 
 class Client
 {
-    /**
-     * @var Config
-     */
-    private $config;
+    private const USER_AGENT = 'Ecomail.cz Webareal client (https://github.com/Ecomailcz/webareal-client)';
 
     /**
-     * Webareal api token
+     * Webareal API credentials
      *
-     * @var string|null
+     * @var Credentials
      */
-    private $apiToken;
+    private $credentials;
 
-    public function __construct(string $login, string $pass, string $url, string $apiKey)
+
+    /**
+     * Token cache
+     *
+     * @var ITokenCache
+     */
+    private $tokenCache;
+
+    public function __construct(Credentials $credentials, ?ITokenCache $tokenCache = null)
     {
-        $this->config = new Config($login, $pass, $url, $apiKey);
+        $this->credentials = $credentials;
+        $this->tokenCache = $tokenCache ?? new MemoryCache();
     }
 
-    public function makeRequest(
-        string $httpMethod,
-        string $url,
-        array $postFields = [],
-        array $queryParameters = []
-    ): array {
-        if ($this->apiToken === null) {
-            throw new EcomailWebarealInvalidAuthorization();
-        }
+    public function requestGet(string $urlPath, array $query = []): array
+    {
+        return $this->request('GET', $urlPath, [], $query)->getContent();
+    }
 
-        /** @var resource $ch */
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    public function requestPost(string $urlPath, array $post = [], array $query = []): array
+    {
+        return $this->request('POST', $urlPath, $post, $query)->getContent();
+    }
 
-        curl_setopt($ch, CURLOPT_HTTPAUTH, true);
-        curl_setopt(
-            $ch,
-            CURLOPT_HTTPHEADER,
+    public function requestPut(string $urlPath, array $post = [], array $query = []): array
+    {
+        return $this->request('PUT', $urlPath, $post, $query)->getContent();
+    }
+
+    public function requestDelete(string $urlPath, array $post = [], array $query = []): array
+    {
+        return $this->request('DELETE', $urlPath, $post, $query)->getContent();
+    }
+
+    public function request(string $method, string $urlPath, array $post = [], array $query = []): JsonResponse
+    {
+        $response = $this->processRequest($method, $this->getApiToken(), $urlPath, $post, $query);
+
+        return $response;
+    }
+
+    private function getApiToken(): string
+    {
+        return $this->tokenCache->load() ?? $this->login();
+    }
+
+    private function login(): string
+    {
+        $response = $this->processRequest(
+            'POST',
+            null,
+            'login',
             [
-                'Authorization:  Bearer ' . $this->apiToken,
-                'X-Wa-api-token: ' . $this->config->getApiKey()
+                'username' => $this->credentials->getLogin(),
+                'password' => $this->credentials->getPassword()
             ]
         );
 
-        if (count($queryParameters) !== 0) {
-            $url .= '?' . http_build_query($queryParameters);
+        if (is_string($response->getField('token')) === false) {
+            throw new InvalidResponseException(
+                "Expected 'token' field in response is missing or invalid type", 0, $response
+            );
         }
 
-        curl_setopt($ch, CURLOPT_URL, $this->config->getUrl() . '/' . $url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $httpMethod);
-        curl_setopt(
-            $ch,
-            CURLOPT_USERAGENT,
-            'Ecomail.cz Webareal client (https://github.com/Ecomailcz/webareal-client)'
-        );
+        $this->tokenCache->save($response->getField('token'));
 
-        if (count($postFields) !== 0) {
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postFields));
-        }
-
-        $output = curl_exec($ch);
-
-        if ($output === false) {
-            throw new Exception(curl_error($ch), curl_errno($ch));
-        }
-
-        $result = json_decode($output, true);
-
-        if (curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200 && curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 201) {
-            if (curl_getinfo($ch, CURLINFO_HTTP_CODE) === 404) {
-                throw new EcomailWebarealNotFound();
-            } // Check authorization
-            elseif (curl_getinfo($ch, CURLINFO_HTTP_CODE) === 401) {
-                throw new EcomailWebarealInvalidAuthorization($this->apiToken);
-            } elseif (curl_getinfo($ch, CURLINFO_HTTP_CODE) === 400) {
-                if (isset($result['errors']) && sizeof($result['errors']) > 0) {
-                    foreach ($result['errors'] as $error) {
-                        throw new EcomailShoptetRequestError($error['message']);
-                    }
-                }
-            }
-        }
-
-        if (!$result) {
-            return [];
-        }
-
-        if (array_key_exists('success', $result) && !$result['success']) {
-            throw new EcomailWebrealAnotherError($result);
-        }
-
-        return $result;
+        return $response->getField('token');
     }
 
-    public function getApiToken()
-    {
-        $curl = curl_init();
-        $apiServer = $this->config->getUrl();
-        $username = $this->config->getLogin();
-        $password = $this->config->getPassword();
-        $apiKey = $this->config->getApiKey();
+    private function processRequest(
+        string $method,
+        ?string $apiToken,
+        string $urlPath,
+        array $post = [],
+        array $query = []
+    ): JsonResponse {
+        $requestHeaders = [];
+        $responseHeaders = [];
 
+        if (count($query)) {
+            $urlPath .= (strpos($urlPath, '?') === false ? '?' : '&') . http_build_query($query);
+        }
+
+        $curl = curl_init();
         curl_setopt_array(
             $curl,
             [
-                CURLOPT_URL => "$apiServer/login",
+                CURLOPT_URL => $this->credentials->getUrl() . '/' . $urlPath,
+                CURLOPT_FOLLOWLOCATION => true,
                 CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => "{\n  \"username\": \"$username\",\n  \"password\": \"$password\"\n}",
-                CURLOPT_HTTPHEADER => [
-                    "X-Wa-api-token: $apiKey",
-                ],
+                CURLOPT_FAILONERROR => false,
+                CURLOPT_CUSTOMREQUEST => $method,
+                CURLOPT_USERAGENT => self::USER_AGENT,
+                CURLOPT_HEADERFUNCTION => static function ($curl, $header) use (&$responseHeaders) {
+                    $parts = explode(':', $header, 2);
+                    if (count($parts) === 2) {
+                        $responseHeaders[strtolower(trim($parts[0]))][] = trim($parts[1]);
+                    }
+                    return strlen($header);
+                },
             ]
         );
 
-        $output = curl_exec($curl);
-        $err = curl_error($curl);
-
-
-        if ($output === false) {
-            throw new Exception($err, curl_errno($curl));
+        if (count($post) && in_array(strtoupper($method), ['POST', 'PUT', 'DELETE', 'PATCH'])) {
+            $requestHeaders[] = 'Content-Type: application/json';
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $this->jsonEncode($post));
         }
 
-        $result = json_decode($output);
+        $requestHeaders[] = 'X-Wa-api-token: ' . $this->credentials->getApiKey();
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $requestHeaders);
 
-        return $result;
+        if ($apiToken !== null) {
+            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+            curl_setopt($curl, CURLOPT_XOAUTH2_BEARER, $apiToken);
+        }
+
+        $responseContent = curl_exec($curl);
+        if ($error = curl_error($curl)) {
+            $curl_errno = curl_errno($curl);
+            curl_close($curl);
+            throw new ConnectionException($error, $curl_errno);
+        }
+
+        $response = new Response($responseContent, $responseHeaders, curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        curl_close($curl);
+
+        if ($response->getStatusCode() >= 400) {
+            throw $this->createResponseErrorException($response);
+        }
+
+        if (empty($response->getContent())) {
+            return new JsonResponse([], $response->getHeaders(), $response->getStatusCode());
+        }
+
+        if ($response->getHeaderValue('content-type') !== 'application/json') {
+            $type = $response->getHeaderValue('content-type') ?? 'undefined';
+            throw new UnexpectedResponseException(
+                "Expected JSON response, '{$type}' type response instead",
+                0,
+                $response
+            );
+        }
+
+        try {
+            $data = json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            throw new InvalidResponseException($e->getMessage(), $e->getCode(), $response, $e);
+        }
+
+        return new JsonResponse($data, $response->getHeaders(), $response->getStatusCode());
+    }
+
+    private function jsonEncode(array $data): string
+    {
+        try {
+            return json_encode(
+                $data,
+                JSON_THROW_ON_ERROR
+                | JSON_UNESCAPED_UNICODE
+                | JSON_UNESCAPED_SLASHES
+                | JSON_PRESERVE_ZERO_FRACTION
+            );
+        } catch (JsonException $e) {
+            throw new InvalidRequestException('JSON: ' . $e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    private function createResponseErrorException(Response $response): ResponseErrorException
+    {
+        $map = [
+            401 => UnauthorizedException::class,
+            403 => AccessDeniedException::class,
+            404 => NotFoundException::class,
+        ];
+        $exceptionClass = $map[$response->getStatusCode()] ?? ResponseErrorException::class;
+
+        return new $exceptionClass(
+            "Server returns error status code: {$response->getStatusCode()}",
+            $response->getStatusCode(),
+            $response
+        );
     }
 }
