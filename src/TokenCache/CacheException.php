8afe600f3480496b0262cd22779a940487b9944e
Jakub Bouček <pan@jakubboucek.cz>
2020-05-12 04:24:41 +0200

    Create API Token cache
    Prevent spent unnecessary requests requests to repeated login before token expiration

---
+++ src/TokenCache/CacheException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EcomailWebareal\TokenCache;
+
+use EcomailWebareal\Exception\RuntimeException;
+
+class CacheException extends RuntimeException
+{
+
+}
